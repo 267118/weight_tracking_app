e4e1c98b2c1f70a76eb218e181e007abbb2391b1
Anna Chyzh <267118@glenbrook225.org>
2025-09-11 10:12:59 -0500

    add input

--- Main.java
+++ Main.java
@@ -52,49 +52,6 @@ public class Main {
   
              y += gap;
 
-        //  JLabel day1 = new JLabel("Day 1");
-        //  day1.setBounds(30,10,400,100);
-        //  day1.setPreferredSize(new Dimension(400,300));
-        //  day1.setFont(new Font("Arial", Font.BOLD, 40));
-        //  ff.add(day1);
-        //  JTextField inputField = new JTextField();
-        //  inputField.setBounds(150, 50, 200, 30);
-        //  ff.add(inputField);
-
-        //  JLabel day2 = new JLabel("Day 2");
-        //  day2.setBounds(30,80,400,100);
-        //  day2.setPreferredSize(new Dimension(400,300));
-        //  day2.setFont(new Font("Arial", Font.BOLD, 40));
-        //  ff.add(day2);
-        //  JLabel day3 = new JLabel("Day 3");
-        //  day3.setBounds(30,150,400,100);
-        //  day3.setPreferredSize(new Dimension(400,300));
-        //  day3.setFont(new Font("Arial", Font.BOLD, 40));
-        //  ff.add(day3);
-        //  JLabel day4 = new JLabel("Day 4");
-        //  day4.setBounds(30,220,400,100);
-        //  day4.setPreferredSize(new Dimension(400,300));
-        //  day4.setFont(new Font("Arial", Font.BOLD, 40));
-        //  ff.add(day4);
-        //  JLabel day5 = new JLabel("Day 5");
-        //  day5.setBounds(30,290,400,100);
-        //  day5.setPreferredSize(new Dimension(400,300));
-        //  day5.setFont(new Font("Arial", Font.BOLD, 40));
-        //  ff.add(day5);
-        //  JLabel day6 = new JLabel("Day 6");
-        //  day6.setBounds(30,360,400,100);
-        //  day6.setPreferredSize(new Dimension(400,300));
-        //  day6.setFont(new Font("Arial", Font.BOLD, 40));
-        //  ff.add(day6);   
-        //  JLabel day7 = new JLabel("Day 7");
-        //  day7.setBounds(30,430,400,100);
-        //  day7.setPreferredSize(new Dimension(400,300));
-        //  day7.setFont(new Font("Arial", Font.BOLD, 40));
-        //  ff.add(day7);
-
-
-
-
         }
 
         
